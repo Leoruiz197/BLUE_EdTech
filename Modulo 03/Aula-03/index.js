a3cca0c12d5412d87f0561bb36b9e31047a5826f
Leo Ruiz <ruizruizleo@hotmail.com>
2021-08-18 23:04:49 -0300

    final

--- Modulo 03/Aula-03/index.js
+++ Modulo 03/Aula-03/index.js
@@ -16,12 +16,12 @@ const filmes = [
 // primeira rota, retorna apernas a msg hello, bluemer.
 app.get('/', (req, res) => {
   res.send('Hello, Bluemer!');
-})
+});
 
 // rota dos filmes - primeira rota - listagem dos filmes.
 app.get('/filmes', (req, res) => {
     res.send(filmes);
-})
+});
 
 // rota do filme individual por id
 app.get('/filmes/:id', (req, res) => {
@@ -32,7 +32,7 @@ app.get('/filmes/:id', (req, res) => {
     res.send('Filme não Encontrado');
   }
   res.send(filme);
-})
+});
 
 // rota que cadastra um novo filme.
 // READ - GET
@@ -45,14 +45,35 @@ app.post('/filmes', (req, res) => {
   filmes.push(filme);
 
   res.send(`Filme adicionado com sucesso: ${filme}. 
-  O ID do filme é ${id} e a nota é ${nota}`)
+  O ID do filme é ${id}`)
 });
 
+app.put('/filmes/:id', (req, res) => {
+  const id = req.params.id -1;
+  const filme = req.body.filme;
+  const nomeAnterior = filmes[id];
+  filmes[id] = filme;
+  res.send(`Filme anterior: ${nomeAnterior}, atualizado com sucesso para: ${filme}.`)
+});
 
+app.delete('/filmes/:id', (req, res) => {
+  const id = req.params.id -1;
+  const filme = filmes[id];
+  if(!filme) {
+    res.send('Filme não Encontrado');
+  }
+  delete filmes[id];
+  res.send("Filme excluido com sucesso");
+});
 
-
-
+//sugestao da galera SPLICE
+app.delete('/filmesSplice/:id', (req,res)=>{
+  const id = req.params.id-1;
+  filmes.splice(id,1)
+  //delete filmes[id]
+  res.send("Filme excluido com sucesso.")
+});
 
 app.listen(port, function() {
   console.info(`App rodando na porta http://localhost:${port}/`);
-})
+});
