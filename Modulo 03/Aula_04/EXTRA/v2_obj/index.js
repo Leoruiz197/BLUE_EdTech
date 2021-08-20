d8e5f80d122309c8096573318241464edff991b4
Leoruiz197 <ruizruizleo@hotmail>
2021-08-20 01:39:52 -0300

    verificado

--- Modulo 03/Aula_04/EXTRA/v2_obj/index.js
+++ Modulo 03/Aula_04/EXTRA/v2_obj/index.js
@@ -1,114 +1,100 @@
 // EXTRA CONCESSIONARIA
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 
-const modelos = [];
-const marcas = [];
-const categorias = [];
-const tipoCombustiveis = [];
-const anos = [];
+function Carro(modelo, marca, categoria, tipoCombustivel, ano) {
+  this.modelo = modelo;
+  this.marca = marca;
+  this.categoria = categoria;
+  this.tipoCombustivel = tipoCombustivel;
+  this.ano = ano;
+}
 
+const carros = [];
 
 const msgInicio = [
-    "Bem vindos, caros alunos",
-    "Ola amigos, bem vindo ao servidor da BLUE",
-    "Servidor de javascript BLUE",
-    "Este é um servidor BLUE",
+  "Bem vindos, caros alunos",
+  "Ola amigos, bem vindo ao servidor da BLUE",
+  "Servidor de javascript BLUE",
+  "Este é um servidor BLUE",
 ];
 
 function random() {
-    return Math.floor(Math.random() * msgInicio.length);
-  }
-  console.log(random());
+  return Math.floor(Math.random() * msgInicio.length);
+}
+console.log(random());
 
 app.get("/", (req, res) => {
-    res.send(msgInicio[random()]);
+  res.send(msgInicio[random()]);
 });
 
 app.get("/carros", (req, res) => {
-    res.send(`
-    Modelos: ${modelos},
-    Marcas: ${marcas},
-    Categorias: ${categorias},
-    Combustivel: ${tipoCombustiveis},
-    Ano: ${anos}`);
+  res.send(carros);
 });
 
 app.get("/carros/:id", (req, res) => {
-    const id = req.params.id -1;
+  const id = req.params.id - 1;
 
-    if(!modelos[id] || !marcas[id] || !categorias[id] || !tipoCombustiveis[id] || !anos[id]){
-        res.send("Veiculo não encontrado, tente novamente");
-    }
-    res.send(`
-    Modelo: ${modelos[id]},
-    Marca: ${marcas[id]},
-    Categoria: ${categorias[id]},
-    Combustivel: ${tipoCombustiveis[id]},
-    Ano: ${anos[id]}`);
+  if (!carros[id]) {
+    res.send("Veiculo não encontrado, tente novamente");
+  }
+  res.send(carros[id]);
 });
 
-app.post('/carros', (req, res) => {
-    const modelo = req.body.modelo;
-    const marca = req.body.marca;
-    const categoria = req.body.categoria;
-    const tipoCombustivel = req.body.tipoCombustivel;
-    const ano = req.body.ano;
+app.post("/carros", (req, res) => {
+  const modelo = req.body.modelo;
+  const marca = req.body.marca;
+  const categoria = req.body.categoria;
+  const tipoCombustivel = req.body.tipoCombustivel;
+  const ano = req.body.ano;
 
-    const id = modelos.length;
+  const id = carros.length;
+  const carro = new Carro(modelo, marca, categoria, tipoCombustivel, ano);
 
-    modelos[id] = modelo;
-    marcas[id] = marca;
-    categorias[id] = categoria;
-    tipoCombustiveis[id] = tipoCombustivel;
-    anos[id] = ano;
+  carros[id] = carro;
 
-    res.send(`Veiculo adicionado com sucesso: ${modelo}. 
-    O ID do Veiculo é ${id+1}`)
-  });
+  res.send(`Veiculo adicionado com sucesso: ${modelo}. 
+    O ID do Veiculo é ${id + 1}`);
+});
 
 app.put("/carros/:id", (req, res) => {
-    const id = req.params.id -1;
+  const id = req.params.id - 1;
 
-    if(!modelos[id] || !marcas[id] || !categorias[id] || !tipoCombustiveis[id] || !anos[id]){
-        res.send("Veiculo não encontrado, tente novamente");
-    }
+  if (!carros[id]) {
+    res.send("Veiculo não encontrado, tente novamente");
+  }
 
-    const modelo = req.body.modelo;
-    const modeloAnterior = modelos[id];
-    const marca = req.body.marca;
-    const categoria = req.body.categoria;
-    const tipoCombustivel = req.body.tipoCombustivel;
-    const ano = req.body.ano;
+  const modelo = req.body.modelo;
+  const modeloAnterior = carros[id].modelo;
+  const marca = req.body.marca;
+  const categoria = req.body.categoria;
+  const tipoCombustivel = req.body.tipoCombustivel;
+  const ano = req.body.ano;
 
-    modelos[id] = modelo;
-    marcas[id] = marca;
-    categorias[id] = categoria;
-    tipoCombustiveis[id] = tipoCombustivel;
-    anos[id] = ano;
-    res.send(`Veiculo atualizado com sucesso,  veiculo anterior: ${modeloAnterior}, substituido por: ${modelo}`)
+  carros[id].modelo = modelo;
+  carros[id].marca = marca;
+  carros[id].categoria = categoria;
+  carros[id].tipoCombustivel = tipoCombustivel;
+  carros[id].ano = ano;
+
+  res.send(`Veiculo atualizado com sucesso,  veiculo anterior: ${modeloAnterior}, substituido por: ${modelo}`);
 });
 
 app.delete("/carros/:id", (req, res) => {
-    const id = req.params.id -1;
+  const id = req.params.id - 1;
 
-    if(!modelos[id] || !marcas[id] || !categorias[id] || !tipoCombustiveis[id] || !anos[id]){
-        res.send("Veiculo não encontrado, tente novamente");
-    }
-
-    delete modelos[id];
-    delete marcas[id];
-    delete categorias[id];
-    delete tipoCombustiveis[id];
-    delete anos[id];
-    res.send("Veiculo excluido com sucesso.")
+  if (!carros[id]) {
+    res.send("Veiculo não encontrado, tente novamente");
+  }
+  delete carros[id];
+  res.send("Veiculo excluido com sucesso.");
 });
 
-app.listen(port, function(){
-    console.info(`App rodando na porta http://localhost:${port}/`);
+app.listen(port, function () {
+  console.info(`App rodando na porta http://localhost:${port}/`);
 });
