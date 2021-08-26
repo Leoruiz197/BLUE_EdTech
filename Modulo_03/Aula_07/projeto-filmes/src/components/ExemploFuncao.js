d7721c844bf227ef6cb12afb8c6b36e4db9d7929
Ivonaldo Soares <vonaldojrsoares@gmail.com>
2021-08-25 21:51:41 -0300

    adiciona exemplo de map com funcao

--- Modulo_03/Aula_07/projeto-filmes/src/components/ExemploFuncao.js
+++ Modulo_03/Aula_07/projeto-filmes/src/components/ExemploFuncao.js
@@ -2,16 +2,33 @@ import React, { useState, useEffect } from 'react';
 
 export default function ExemploFuncao() {
   const [count, setCount] = useState(0);
+  const [lista, setLista] = useState([
+    {
+      nome: 'ivonaldo',
+      sobrenome: 'soares',
+    },
+    {
+      nome: 'Leo',
+      sobrenome: 'Ruiz',
+    }
+  ]);
 
   // use effect aceita 2 parametros - primeiro a funcao que vai ser executada, e depois qual variavel ele precisa analisar o comportamento.
   useEffect(() => {
     document.title = count;
   }, [count])
 
-  return(
+  return (
     <div>
       <p>{count}</p>
       <button onClick={() => setCount(count + 1)}>Adiciona</button>
+      <ul>
+        {lista.map((lista, index) => (
+          <li key={index}>
+            <p>{lista.nome} {lista.sobrenome}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
