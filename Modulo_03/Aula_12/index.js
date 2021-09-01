536502ddaaba20de6f9e36b06aa46e53891a2ddb
Leoruiz197 <ruizruizleo@hotmail>
2021-08-31 22:29:53 -0300

    metade do final da aula

--- Modulo_03/Aula_12/index.js
+++ Modulo_03/Aula_12/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const Conn = require('./models/conn/conn');
 
--- Modulo_03/Aula_12/routers/musicas.routes.js
+++ Modulo_03/Aula_12/routers/musicas.routes.js
@@ -2,12 +2,44 @@ const express = require('express');
 const router = express.Router();
 const Musica = require('../models/musicas');
 
-router.post("/add", (req, res) => {
-    res.send("rota /add musica");
+router.post("/add", async (req, res) => {
+    await Musica.create(req.body)
+    .then(() => {
+        res.status(200).send("Musica adicionada com sucesso");
+    }).catch((err) => {
+        res.status(400).send("Algo errado com a musica, tente novamente");
+        console.error(err);
+    })
 });
 
-router.get('/', (req, res) => {
-    res.send("rota ok / musica");
+router.get('/', async (req, res) => {
+    await Musica.find({})
+    .then((musica) => {
+        res.send(musica);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+});
+
+router.get('/findById/:id', async (req, res) => {
+    await Musica.find({_id : req.params.id})
+    .then((musica) => {
+        res.send(musica);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+});
+
+router.get('/findByName/:name', async (req, res) => {
+    await Musica.find({nome : req.params.name})
+    .then((musica) => {
+        res.send(musica);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 });
 
 router.put("/update", (req, res) => {
