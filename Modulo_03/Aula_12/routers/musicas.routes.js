5177b0fb41101c7e39bdb089a17af45643f559eb
Leoruiz197 <ruizruizleo@hotmail>
2021-08-31 22:58:58 -0300

    CRUD completo API

--- Modulo_03/Aula_12/routers/musicas.routes.js
+++ Modulo_03/Aula_12/routers/musicas.routes.js
@@ -15,9 +15,10 @@ router.post("/add", async (req, res) => {
 router.get('/', async (req, res) => {
     await Musica.find({})
     .then((musica) => {
-        res.send(musica);
+        res.status(200).send(musica);
     })
     .catch((err) => {
+        res.status(400).send("Algo errado com a musica, tente novamente");
         console.log(err);
     })
 });
@@ -25,9 +26,10 @@ router.get('/', async (req, res) => {
 router.get('/findById/:id', async (req, res) => {
     await Musica.find({_id : req.params.id})
     .then((musica) => {
-        res.send(musica);
+        res.status(200).send(musica);
     })
     .catch((err) => {
+        res.status(400).send("Algo errado com a musica, tente novamente");
         console.log(err);
     })
 });
@@ -35,19 +37,34 @@ router.get('/findById/:id', async (req, res) => {
 router.get('/findByName/:name', async (req, res) => {
     await Musica.find({nome : req.params.name})
     .then((musica) => {
-        res.send(musica);
+        res.status(200).send(musica);
     })
     .catch((err) => {
+        res.status(400).send("Algo errado com a musica, tente novamente");
         console.log(err);
     })
 });
 
-router.put("/update", (req, res) => {
-    res.send("rota /update musica");
+router.put("/update/:id", async (req, res) => {
+    await Musica.updateOne({_id : req.params.id},req.body)
+    .then(() => {
+        res.status(200).send("Atualizado com sucesso");
+    })
+    .catch((err) => {
+        res.status(400).send("Algo errado com a musica, tente novamente");
+        console.log(err);
+    });
 });
 
-router.delete("/delete", (req, res) => {
-    res.send("rota /delete musica");
+router.delete("/delete/:id", async (req, res) => {
+    await Musica.deleteOne({_id : req.params.id})
+    .then(() => {
+        res.status(200).send("Deletado com sucesso");
+    })
+    .catch((err) => {
+        res.status(400).send("Algo errado com a musica, tente novamente");
+        console.log(err);
+    });
 });
 
 
